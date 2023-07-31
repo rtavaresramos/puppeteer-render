6aade8f09911d8a6a42b7967c853f3e606694703
rtavaresramos <rtavareskxd99@gmail.com>
2023-07-31 19:29:24 +0200

    first commit

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,8 +1,16 @@
-const puppeteer = require("puppeteer");
+
+const puppeteer = require('puppeteer-extra')
 require("dotenv").config();
 
-const scrapeLogic = async (res) => {
-  const browser = await puppeteer.launch({
+// Add stealth plugin and use defaults 
+const pluginStealth = require('puppeteer-extra-plugin-stealth')
+const { executablePath } = require('puppeteer');
+
+// Use stealth 
+puppeteer.use(pluginStealth())
+
+const scrapeLogic = (res) => {
+  puppeteer.launch({
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
@@ -12,40 +20,54 @@ const scrapeLogic = async (res) => {
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-  });
-  try {
+        : executablePath(),
+  }).then(async browser => {
+    // Create a new page 
     const page = await browser.newPage();
+    try {
+      // Setting page view 
+      await page.setViewport({ width: 1280, height: 720 });
 
-    await page.goto("https://developer.chrome.com/");
+      // Go to the website 
+      await page.goto('https://otv.verwalt-berlin.de/ams/TerminBuchen?lang=en');
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
+      const nxtButton = '.ui-button.ui-widget.ui-state-default.ui-corner-all.ui-button-text-only.button.arrow-right'
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+      // Wait for security check 
+      // await page.waitForTimeout(4000);
+      await page.click('.link > .button');
+      await page.waitForNavigation()
+      await page.click('.XItem.XCheckbox.left-right')
+      await page.click(nxtButton)
+      await page.waitForNavigation()
+      await page.waitForTimeout(3000);
+      await page.select('#xi-sel-400', '327')
+      await page.select('#xi-sel-422', '2')
+      await page.select('#xi-sel-427', '1')
+      await page.select('#xi-sel-428', '327-0')
+      await page.waitForTimeout(3000);
+      await page.click('.ozg-kachel.kachel-327-0-2.level1')
+      await page.waitForTimeout(3000);
+      await page.click('.ozg-accordion.accordion-327-0-2-1.level2')
+      await page.waitForTimeout(3000);
+      await page.click('#SERVICEWAHL_EN327-0-2-1-324659')
+      await page.waitForTimeout(3000);
+      await page.click(nxtButton)
+      await page.waitForNavigation()
+      await page.waitForTimeout(3000);
 
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+      const image = await page.screenshot();
 
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
-  } catch (e) {
-    console.error(e);
-    res.send(`Something went wrong while running Puppeteer: ${e}`);
-  } finally {
-    await browser.close();
-  }
+      res.set('Content-Type', 'image/png');
+      res.send(image);
+    } catch (e) {
+      console.error(e);
+      res.send(`Something went wrong while running Puppeteer: ${e}`);
+    }
+    finally {
+      await browser.close();
+    }
+  })
 };
 
 module.exports = { scrapeLogic };
